0b23c34c67837a4aa11cae497f26fd4ef8e291f5
Arseni Skobelev <90300699+ArseniSkobelev@users.noreply.github.com>
2022-09-23 13:29:32 +0200

    Update create-alias.sh

--- create-alias.sh
+++ create-alias.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
   cat >> ~/.bashrc << EOT
 blazortailwind() 
 {
-    powershell -File "${SCRIPT_DIR}/dotnet-blazor-tailwind.ps1 {$1} {$2}"
+    powershell -File "${SCRIPT_DIR}/dotnet-blazor-tailwind.ps1" $1 $2
 }
 EOT
 
